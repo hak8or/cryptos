2d8ae559dbc5f5f358a014e1b1619f877f3ae645
hak8or <hak8or@gmail.com>
2014-01-26 16:56:52 -0500

    Tmux, tmuxinator, and tmuxinator project added.
    Redis was also moved on over into startup.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # ---- Todo in this script ----
-# New script for starting up services.
-# Add tmuxinator and tmux installation for startup script.
+# -New script for starting up services.
+# -Add tmuxinator and tmux installation for startup script.
+# The above is combined into this: make a tmuxinator project which starts up all services.
 
 echo "+----------------------------------------------------+"
 echo "|  Setup of the cryptos website used to track        |"
@@ -26,22 +27,166 @@ setup_by_my_script=0
 project_directory=$working_directory/cryptos
 log_location="$working_directory/setup.log"
 
+# Go inside the working directory where all the downloading will be going on.
+cd $working_directory
+
 # Setup logging file and location which will hold this scripts output.
   mkdir -p $working_directory
   touch $log_location &>>/dev/null
   echo "----FROM SCRIPT ECHO---- Starting Script" &>>$log_location
 
 # Install the required packages
-  echo "  [1/5] Installing required packages"
+  echo "  [1/6] Installing required packages"
   echo "----FROM SCRIPT ECHO---- Installing required packages" &>>$log_location
 
   # Install git to download cryptos. Maybe make releases?
-  echo "    \ [1/1] git to clone the cryptos git repo"
+  echo "    |- [1/6] git to clone the cryptos git repo"
   echo "----FROM SCRIPT ECHO---- git to clone the cryptos git repo" &>>$log_location
   sudo apt-get install -y git &>>$log_location
 
+  # Install the redis-server used by sidekiq.
+    echo "    |- [2/6] Installing Redis Server for sidekiq"
+    echo "----FROM SCRIPT ECHO---- Installing Redis Server" &>>$log_location
+
+    echo "         |- [1/5] Downloading redis tarball"
+    echo "----FROM SCRIPT ECHO---- Downloading redis tarball" &>>$log_location
+    wget http://download.redis.io/redis-stable.tar.gz &>>$log_location
+
+    echo "         |- [2/5] Extracting redis tarball"
+    echo "----FROM SCRIPT ECHO---- Extracting redis tarball" &>>$log_location
+    tar -xvzf redis-stable.tar.gz &>>$log_location
+
+    echo "         |- [3/5] Compiling redis"
+    echo "----FROM SCRIPT ECHO---- Compiling redis" &>>$log_location
+    cd redis-stable
+    make &>>$log_location
+
+    # Copy redis-server over to bin so you can just run redis-server anywhere.
+    echo "         |- [4/5] Copying redis-server and redis-cli"
+    echo "----FROM SCRIPT ECHO---- Copying redis-server and redis-cli" &>>$log_location
+    sudo cp src/redis-server /usr/local/bin/ &>>$log_location
+
+    # Same but for the CLI (Command Line Interface)
+    sudo cp src/redis-cli /usr/local/bin/ &>>$log_location
+
+    # Go back to working directory.
+    cd $working_directory &>>$log_location
+
+    # Delete the redis-stable folder and tarbal since we don't need them anymore.
+    echo "         \- [5/5] Cleaning up after myself"
+    echo "----FROM SCRIPT ECHO---- Cleaning up after myself" &>>$log_location
+    rm -r -f redis-stable &>>$log_location
+    rm redis-stable.tar.gz &>>$log_location
+
+  # Install libevent for tmux
+    echo "    |- [3/6] Install libevent for tmux"
+    echo "----FROM SCRIPT ECHO---- Install libevent for tmux" &>>$log_location
+
+    echo "         |- [1/6] Downloading libevent tarball"
+    echo "----FROM SCRIPT ECHO---- Downloading libevent tarball" &>>$log_location
+    wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz &>>$log_location
+
+    echo "         |- [2/6] Extracting libevent tarball"
+    echo "----FROM SCRIPT ECHO---- Extracting libevent tarball" &>>$log_location
+    tar -xvzf libevent-2.0.21-stable.tar.gz &>>$log_location
+
+    echo "         |- [3/6] Configuring libevent"
+    echo "----FROM SCRIPT ECHO---- Configuring libevent" &>>$log_location
+    cd libevent-2.0.21-stable
+    ./configure &>>$log_location
+
+    echo "         |- [4/6] Compiling libevent"
+    echo "----FROM SCRIPT ECHO---- Compiling libevent" &>>$log_location
+    make &>>$log_location
+
+    echo "         |- [5/6] Installing libevent"
+    echo "----FROM SCRIPT ECHO---- Installing libevent" &>>$log_location
+    sudo make install &>>$log_location
+
+    # Go back to working directory.
+    cd $working_directory &>>$log_location
+
+    echo "         \- [6/6] Cleaning up after myself"
+    echo "----FROM SCRIPT ECHO---- Cleaning up after myself" &>>$log_location
+    rm -r -f libevent-2.0.21-stable &>>$log_location
+    rm libevent-2.0.21-stable.tar.gz &>>$log_location
+
+  # Install ncurses for tmux
+    echo "    |- [4/6] Install ncurses for tmux"
+    echo "----FROM SCRIPT ECHO---- Install ncurses for tmux" &>>$log_location
+
+    echo "         |- [1/6] Downloading ncurses tarball"
+    echo "----FROM SCRIPT ECHO---- Downloading ncurses tarball" &>>$log_location
+    wget http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz &>>$log_location
+
+    echo "         |- [2/6] Extracting ncurses tarball"
+    echo "----FROM SCRIPT ECHO---- Extracting ncurses tarball" &>>$log_location
+    tar -xvzf ncurses-5.9.tar.gz &>>$log_location
+
+    echo "         |- [3/6] Configuring ncurses"
+    echo "----FROM SCRIPT ECHO---- Configuring ncurses" &>>$log_location
+    cd ncurses-5.9
+    ./configure &>>$log_location
+
+    echo "         |- [4/6] Compiling ncurses"
+    echo "----FROM SCRIPT ECHO---- Compiling ncurses" &>>$log_location
+    make &>>$log_location
+
+    echo "         |- [5/6] Installing ncurses"
+    echo "----FROM SCRIPT ECHO---- Installing ncurses" &>>$log_location
+    sudo make install &>>$log_location
+
+    # Go back to working directory.
+    cd $working_directory &>>$log_location
+
+    echo "         \- [6/6] Cleaning up after myself"
+    echo "----FROM SCRIPT ECHO---- Cleaning up after myself" &>>$log_location
+    rm -r -f ncurses-5.9 &>>$log_location
+    rm ncurses-5.9.tar.gz &>>$log_location
+
+  # Install tmux
+    echo "    |- [5/6] Install ncurses for tmux"
+    echo "----FROM SCRIPT ECHO---- Install tmux" &>>$log_location
+
+    echo "         |- [1/6] Downloading tmux tarball"
+    echo "----FROM SCRIPT ECHO---- Downloading tmux tarball" &>>$log_location
+    wget http://downloads.sourceforge.net/tmux/tmux-1.8.tar.gz &>>$log_location
+
+    echo "         |- [2/6] Extracting tmux tarball"
+    echo "----FROM SCRIPT ECHO---- Extracting tmux tarball" &>>$log_location
+    tar -xvzf tmux-1.8.tar.gz &>>$log_location
+
+    echo "         |- [3/6] Configuring tmux"
+    echo "----FROM SCRIPT ECHO---- Configuring tmux" &>>$log_location
+    cd tmux-1.8
+    ./configure &>>$log_location
+
+    echo "         |- [4/6] Compiling tmux"
+    echo "----FROM SCRIPT ECHO---- Compiling tmux" &>>$log_location
+    make &>>$log_location
+
+    echo "         |- [5/6] Installing tmux"
+    echo "----FROM SCRIPT ECHO---- Installing tmux" &>>$log_location
+    sudo make install &>>$log_location
+
+    # Go back to working directory.
+    cd $working_directory &>>$log_location
+
+    echo "         \- [6/6] Cleaning up after myself"
+    echo "----FROM SCRIPT ECHO---- Cleaning up after myself" &>>$log_location
+    rm -r -f tmux-1.8-5.9 &>>$log_location
+    rm tmux-1.8.tar.gz &>>$log_location
+
+  # Install tmuxinator plugin
+    echo "    |- [6/6] Install tmuxinator plugin for tmux"
+    echo "----FROM SCRIPT ECHO---- Install tmuxinator plugin for tmux" &>>$log_location
+    sudo gem install tmuxinator &>>$log_location
+
+    # Configure what editor tmuxinator should use as default.
+    export EDITOR=nano
+
 # Downloading the github repo section.
-  echo "  [2/5] Fetching cryptos git repo"
+  echo "  [2/6] Fetching cryptos git repo"
   echo "----FROM SCRIPT ECHO---- Fetching cryptos git repo" &>>$log_location
   cd $working_directory &>>$log_location
 
@@ -80,57 +225,8 @@ log_location="$working_directory/setup.log"
 
   fi
 
-# Install the redis-server used by sidekiq.
-  echo "  [3/5] Installing Redis Server"
-  echo "----FROM SCRIPT ECHO---- Installing Redis Server" &>>$log_location
-
-  echo "    |- [1/5] Downloading redis tarball"
-  echo "----FROM SCRIPT ECHO---- Downloading redis tarball" &>>$log_location
-  wget http://download.redis.io/redis-stable.tar.gz &>>$log_location
-
-  echo "    |- [2/5] Extracting redis tarball"
-  echo "----FROM SCRIPT ECHO---- Extracting redis tarball" &>>$log_location
-  tar xvzf redis-stable.tar.gz &>>$log_location
-
-  echo "    |- [3/5] Compiling redis"
-  echo "----FROM SCRIPT ECHO---- Compiling redis" &>>$log_location
-  cd redis-stable
-  make &>>$log_location
-
-  # Copy redis-server over to bin so you can just run redis-server anywhere.
-  echo "    |- [4/5] Copying redis-server and redis-cli"
-  echo "----FROM SCRIPT ECHO---- Copying redis-server and redis-cli" &>>$log_location
-  sudo cp src/redis-server /usr/local/bin/ &>>$log_location
-
-  # Same but for the CLI (Command Line Interface)
-  sudo cp src/redis-cli /usr/local/bin/ &>>$log_location
-
-  # Lets see if redis-server is working correctly.
-  # I do not have a good solution for starting processes which take control
-  # of the terminal and run forever, so this test is commented out for now.
-  # I hope to either try using supervisord for this or just temporarily
-  # start and stop it just to tests is redis-server is ok.
-    # response=$(redis-cli ping)
-
-    #if [[response -eq "pong"]]
-    #  echo "Redis Server working correctly!"
-    #else
-    #  echo "Redis Server failed to respond with ping."
-    #  echo "This means that Redis Server is not working correctly."
-    #  exit
-    #fi
-
-  # Go back to working directory.
-  cd $working_directory &>>$log_location
-
-  # Delete the redis-stable folder and tarbal since we don't need them anymore.
-  echo "    \- [5/5] Cleaning up after myself"
-  echo "----FROM SCRIPT ECHO---- Cleaning up after myself" &>>$log_location
-  rm -r -f redis-stable &>>$log_location
-  rm redis-stable.tar.gz &>>$log_location
-
 # Configures the cryptos rails application.
-  echo "  [4/5] Setting up Cryptos"
+  echo "  [4/6] Setting up Cryptos"
   echo "----FROM SCRIPT ECHO---- Setting up Cryptos" &>>$log_location
 
   # Add in therubyracer gem as the JS runtime to the Gemfile.
@@ -184,9 +280,29 @@ _EOF_
   sudo bundle exec rake db:migrate &>>$log_location
 
 # Restart the Nginx server to apply changes to database.yml
-  echo "  [5/5] Restarting Nginx for changes to take effect"
+  echo "  [5/6] Restarting Nginx for changes to take effect"
   echo "----FROM SCRIPT ECHO---- Restarting Nginx for changes to take effect" &>>$log_location
   sudo service nginx restart &>>$log_location
+
+# If the user wishes to be able to start up the cryptos site and all the required services,
+# all that is needed is a simple tmuxinator cryptos command and everything is up and running.
+  echo "  [6/5] Generating tmuxinator profile"
+  echo "----FROM SCRIPT ECHO---- Generating tmux profile for cryptos and stack" &>>$log_location
+
+  cat <<- _EOF_ >$working_directory/.tmuxinator/cryptos.yml
+
+# ~/.tmuxinator/cryptos.yml
+name: cryptos
+root: $project_directory
+
+windows:
+  - server:
+      layout: main-vertical
+      panes:
+        - redis-server
+        - bundle exec sidekiq
+        - bundle exec clockwork app/clock.rb
+_EOF_
 
 echo "----FROM SCRIPT ECHO---- Script done!" &>>$log_location
 echo "+----------------------------------------------------+"
